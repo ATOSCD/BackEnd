60e2f070e2f08a20c65b00d1abb10ef72c919d91
ippinex <ippiex0913@naver.com>
2025-04-06 17:23:14 +0900

    (DOCS) Add description and tags to API endpoints

--- app/main.py
+++ app/main.py
@@ -22,22 +22,27 @@ def get_db():
     finally:
         db.close()
 
+# FastAPI 기본 경로
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 # 사용자 추가 API
-@app.post("/add-user/")
+@app.post("/add-user/", description="사용자 추가", tags=["User"])
 def add_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db, user)
 
 # 모든 사용자 조회 API
-@app.get("/get-users/")
+@app.get("/get-users/", description="모든 사용자 조회", tags=["User"])
 def read_users(db: Session = Depends(get_db)):
     return get_users(db)
 
 # 버튼 로그 추가 API
-@app.post("/add-button-log/")
+@app.post("/add-button-log/", description="버튼 클릭 로그 추가", tags=["Button"])
 def button_log_add(button_log: ButtonLogAdd, db: Session = Depends(get_db)):
     return add_button_log(db, button_log)
 
 # 버튼 추천 API
-@app.post("/recommend-button/")
+@app.post("/recommend-button/", description="버튼 추천 (최근 1주일 가장 많은 순서대로 리턴)", tags=["Button"])
 def button_recommend(recommend: ButtonRecommend, db: Session = Depends(get_db)):
     return recommend_buttons(db, recommend)
