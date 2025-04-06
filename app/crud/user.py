030e75fb731aeb2a64c344b813b3a7eeca66d658
ippinex <ippiex0913@naver.com>
2025-04-06 16:04:34 +0900

    (REFACTOR) crud user

--- app/crud/user.py
+++ app/crud/user.py
@@ -3,7 +3,11 @@ from app.models.User import User
 from app.schemas.user import UserCreation
 
 def create_user(db: Session, user: UserCreation):
-    db_user = User(user_id=user.user_id, name=user.name, password=user.password)
+    db_user = User(
+        user_id=user.user_id, 
+        name=user.name, 
+        password=user.password
+    )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
