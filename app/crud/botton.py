9d9905bdedf1e1c2bff93363f47e643ac2179f91
ippinex <ippiex0913@naver.com>
2025-04-06 16:09:28 +0900

    (FEAT) Add button log, button recommend

---
+++ app/crud/botton.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from app.models.User import User
+from datetime import datetime, timedelta
+from sqlalchemy import func, desc
+from app.models.Button import ButtonLog
+from app.schemas.button import ButtonLogAdd, ButtonRecommend
+
+def add_button_log(db: Session, button_log: ButtonLogAdd):
+    log = ButtonLog(
+        user_id=button_log.user_id,
+        button_id=button_log.button_id,
+        date=datetime.now()
+    )
+    db.add(log)
+    db.commit()
+    db.refresh(log)
+    return button_log
+
+
+def recommend_buttons(db: Session, recommend: ButtonRecommend):
+    week = datetime.now() - timedelta(days=7)
+
+    # 쿼리 작성: 최근 1주일간의 button_id를 개수로 정렬
+    result = (
+        db.query(ButtonLog.button_id, func.count(ButtonLog.button_id).label("count"))
+        .filter(ButtonLog.date >= week)                  # 최근 1주일 데이터 필터링
+        .filter(ButtonLog.user_id == recommend.user_id)  # 특정 사용자 필터링
+        .group_by(ButtonLog.button_id)                   # button_id로 그룹화
+        .order_by(desc("count"))                         # 개수로 내림차순 정렬
+        .all()
+    )
+
+    return [row.button_id for row in result]
---
+++ app/schemas/button.py
@@ -0,0 +1,6 @@
+def ButtonLogAdd():
+    user_id: str
+    button_id: int
+
+def ButtonRecommend():
+    user_id: str
