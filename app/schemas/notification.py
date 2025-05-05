2c6db4b8d3a9dfff87c25ca5f4ef69ac2a85b41d
ippinex <ippiex0913@naver.com>
2025-05-05 15:47:04 +0900

    (FEAT) Add NotificationResponse schema

--- app/schemas/notification.py
+++ app/schemas/notification.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class NotificationRequest(BaseModel):
     user_id: str
@@ -8,3 +9,11 @@ class NotificationRequest(BaseModel):
 class TokenData(BaseModel):
     user_id: str
     token: str
+
+class NotificationResponse(BaseModel):
+    id: int
+    user_id: str
+    title: str
+    body: str
+    created_at: datetime
+
